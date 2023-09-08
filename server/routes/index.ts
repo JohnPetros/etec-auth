c20c7f03c623476106b2c7bff5f5766ddb041aa7
João Pedro Carvalho <joaopedro.nc@outlook.com>
2023-09-08 23:54:17 +0000

    🚚 Organize folders

--- server/routes/index.ts
+++
@@ -1,9 +0,0 @@
-import { Router } from 'express'
-
-import { AuthController } from '../controllers/AuthController'
-
-export const routes = Router()
-
-const authController = new AuthController()
-
-routes.post('/sign_up', authController.signUp)
