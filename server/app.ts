505d5d1a8ff2b90603bfaf8748003f755eddce5d
João Pedro Carvalho <joaopedro.nc@outlook.com>
2023-09-08 03:32:40 +0000

    🛣️ Use routes

--- server/app.ts
+++ server/app.ts
@@ -2,6 +2,10 @@ import express from 'express'
 import 'dotenv/config'
 import './database'
 
+import { routes } from './routes'
+
 const app = express()
+
+app.use('/auth', routes)
 
 app.listen(3333, () => console.log('Server is Running!'))
