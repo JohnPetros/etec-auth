7a7911a3ea895f61d8f3162c3c6b1abd68247c67
João Pedro Carvalho <joaopedro.nc@outlook.com>
2023-09-09 15:55:27 +0000

    🏷️ Add UsersRepository Interface

---
+++ server/src/repositories/interfaces/IUsersRepository.ts
@@ -0,0 +1,8 @@
+import { User } from '../../entities/User'
+import { CreateUserDTO } from '../../dtos/CreateUserDTO'
+
+export interface IUsersRepository {
+  findById(id: string): Promise<User | null>
+  findByEmail(id: string): Promise<User | null>
+  create({ name, email, password }: CreateUserDTO): Promise<User>
+}
