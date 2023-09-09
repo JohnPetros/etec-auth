c49312218b3e143d33eea66f7382cfdbda449f99
João Pedro Carvalho <joaopedro.nc@outlook.com>
2023-09-09 16:35:58 +0000

    🔒 Validate if provided email already exists

--- server/src/useCases/CreateUser.ts
+++ server/src/useCases/CreateUser.ts
@@ -1,5 +1,6 @@
 import { User } from '../entities/User'
 import { IUsersRepository } from '../repositories/interfaces/IUsersRepository'
+import { AppError } from '../utils/AppError'
 
 interface Request {
   name: string
@@ -18,6 +19,10 @@ export class CreateUserUseCase {
     password_confirmation,
   }: Request): Promise<User> {
     const userAlreadyExists = await this.usersRepository.findByEmail(email)
+
+    if (userAlreadyExists) {
+      throw new AppError('Email already in use', 409)
+    }
 
     const createdUser = await this.usersRepository.create({
       name,
