e423acc234e956e3fa34f05a3cab2ff0a824d3c9
João Pedro Carvalho <joaopedro.nc@outlook.com>
2023-09-09 16:19:46 +0000

    🔧 chore: Add handleError middleware to app config

--- server/src/configs/appConfig.ts
+++ server/src/configs/appConfig.ts
@@ -1,9 +1,13 @@
 import express, { Express } from 'express'
 import cors from 'cors'
 import { routes } from '../routes'
+import { handleError } from '../middlewares/handleError'
 
 export function appConfig(app: Express): void {
   app.use(express.json())
+
   // app.use(cors)
   app.use('/auth', routes)
+
+  app.use(handleError)
 }
