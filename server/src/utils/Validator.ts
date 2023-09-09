e90abbdc3427bf2e6f0b59f4a0f4d056e72f6cdd
João Pedro Carvalho <joaopedro.nc@outlook.com>
2023-09-09 17:42:27 +0000

    🧰 Add Validator util

---
+++ server/src/utils/Validator.ts
@@ -0,0 +1,53 @@
+import { ZodError, z } from 'zod'
+import { AppError } from './AppError'
+
+export class Validator {
+  private readonly passwordRegex =
+    /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[^\w\s])[A-Za-z\d\W\S]{6,}$/g
+
+  private signUpUserSchema = z
+    .object({
+      name: z.string().min(3),
+      email: z.string().email('E-mail está formato incorreto'),
+      password: z
+        .string()
+        .refine(
+          (password) => this.passwordRegex.test(password),
+          'Senha está formato incorreto'
+        ),
+      password_confirmation: z.string(),
+    })
+    .refine(
+      ({ password, password_confirmation }) =>
+        password === password_confirmation,
+      'Senhas não conferem'
+    )
+
+  execute(validation: () => void) {
+    try {
+      validation()
+    } catch (error) {
+      throw new AppError(this.formatError(error as ZodError), 400)
+    }
+  }
+
+  formatError(error: ZodError) {
+    return error.issues.map((error) => error.message)
+  }
+
+  validateSignUpUser({
+    name,
+    email,
+    password,
+    password_confirmation,
+  }: z.infer<typeof this.signUpUserSchema>) {
+    return this.execute(() =>
+      this.signUpUserSchema.parse({
+        name,
+        email,
+        password,
+        password_confirmation,
+      })
+    )
+  }
+}
