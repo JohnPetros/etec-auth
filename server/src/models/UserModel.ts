a1cc5f0342cb43f791492a020b22a1c4be69fc9a
João Pedro Carvalho <joaopedro.nc@outlook.com>
2023-09-09 15:57:08 +0000

    🗄️ Add User model

---
+++ server/src/models/UserModel.ts
@@ -0,0 +1,27 @@
+import { Model, Schema, model } from 'mongoose'
+import { User } from '../entities/User'
+import normalize from 'normalize-mongoose'
+
+const UserSchema = new Schema<User>({
+  id: {
+    type: String,
+    index: true,
+  },
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+})
+
+UserSchema.plugin(normalize)
+
+export const UserModel: Model<User> = model('user', UserSchema)
