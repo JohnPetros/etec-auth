bf43e20a296b56b8e1a53dba17c7daabefa3d846
João Pedro Carvalho <joaopedro.nc@outlook.com>
2023-09-15 20:09:34 +0000

    ✨ feat (hook): add useAuth hook

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -2,8 +2,9 @@ import { StatusBar } from 'react-native'
 import { GestureHandlerRootView } from 'react-native-gesture-handler'
 import { GluestackUIProvider } from '@gluestack-ui/themed'
 
-import { SignUpForm } from './src/components/SignUpForm'
-import { ToastProvider } from 'react-native-toast-notifications'
+import { AuthProvider } from './src/contexts/AuthContext'
+import { ToastProvider } from './src/contexts/ToastContext'
+import { Pages } from './src/components/Pages'
 
 export default function App() {
   return (
@@ -15,7 +16,11 @@ export default function App() {
           translucent
         />
 
-        <SignUpForm />
+        <ToastProvider>
+          <AuthProvider>
+            <Pages />
+          </AuthProvider>
+        </ToastProvider>
       </GestureHandlerRootView>
     </GluestackUIProvider>
   )
