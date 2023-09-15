50ee62b65850b784ac9f73960464c97b4e16a295
João Pedro Carvalho <joaopedro.nc@outlook.com>
2023-09-15 19:47:18 +0000

    🧰 Add getApiErrorMessage function

--- mobile/src/services/api.ts
+++ mobile/src/services/api.ts
@@ -2,3 +2,7 @@ import { API_URL } from '@env'
 import axios from 'axios'
 
 export const api = axios.create({ baseURL: API_URL })
+
+export const getApiErrorMessage = (error: unknown): string => {
+  return axios.isAxiosError(error) ? error.response?.data.errorMessage : null
+}
