d512a5c967a581d1fa939d6423222d5f67f68c49
João Pedro Carvalho <joaopedro.nc@outlook.com>
2023-09-15 20:56:27 +0000

    ✨ feat (libs): Add zod schemas

---
+++ mobile/src/libs/zod.ts
@@ -0,0 +1,45 @@
+import { z } from 'zod'
+
+const passwordRegex =
+  /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[^\w\s])[A-Za-z\d\W\S]{6,}$/g
+
+const emailSchema = z
+  .string({
+    required_error: 'Insira um e-mail',
+  })
+  .email('E-mail está no formato incorreto')
+
+const passwordSchema = z
+  .string({
+    required_error: 'Insira uma senha',
+  })
+  .regex(
+    passwordRegex,
+    'Senha deve ter pelo menos 6 caracteres, sendo 1 caractere especial, 1 minúscula e 1 maiúscula e 1 número'
+  )
+
+export const signUpFormSchema = z
+  .object({
+    name: z
+      .string({
+        required_error: 'Insira um nome de usuário',
+      })
+      .min(3, 'Nome de usuário deve ter pelo menos 3 caracteres'),
+    email: emailSchema,
+    password: passwordSchema,
+    password_confirmation: z.string({
+      required_error: 'Confirme sua senha',
+    }),
+  })
+  .refine(
+    ({ password, password_confirmation }) => password === password_confirmation,
+    'Senhas não conferem'
+  )
+
+export const signInFormSchema = z.object({
+  email: emailSchema,
+  password: passwordSchema,
+})
+
+export type SignUpFormData = z.infer<typeof signUpFormSchema>
+export type SignInFormData = z.infer<typeof signInFormSchema>
