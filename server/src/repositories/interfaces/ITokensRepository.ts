072a22eeba11442cfda499b173f3519f63da55b5
João Pedro <joaopedro.nc@outlook.com>
2023-09-17 16:45:08 -0300

    🏷️ type: add Tokens Repository interface

---
+++ server/src/repositories/interfaces/ITokensRepository.ts
@@ -0,0 +1,8 @@
+import { Token } from '../../entities/Token'
+import { CreateTokenDTO } from '../../dtos/CreateTokenDTO'
+
+export interface ITokensRepository {
+  create({ content, expires_in, user_id }: CreateTokenDTO): Promise<Token | null>
+  findByUserId(user_id: string): Promise<Token | null>
+  deleteById(id: string): Promise<void>
+}
