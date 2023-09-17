921cb8e30bbaf734f6174763ff46c468d8d38cdc
João Pedro <joaopedro.nc@outlook.com>
2023-09-17 13:57:20 -0300

    ✨ feat: (models): add course model

---
+++ server/src/models/CourseModel.ts
@@ -0,0 +1,33 @@
+import { Model, Schema, model } from 'mongoose'
+import normalize from 'normalize-mongoose'
+import { Course } from '../entities/Course'
+
+const CourseSchema = new Schema<Course>({
+  id: {
+    type: String,
+    index: true,
+  },
+  title: {
+    type: String,
+    required: true,
+  },
+  subjects: [{
+    type: Schema.Types.ObjectId,
+    ref: 'subject',
+    required: true,
+  }]
+})
+
+CourseSchema.plugin(normalize)
+
+CourseSchema.set('toObject', {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+  },
+})
+
+
+
+export const CourseModel: Model<Course> = model('course', CourseSchema)
