4c008086c72b7487d3b93061e89c87a177a763bd
João Pedro <joaopedro.nc@outlook.com>
2023-09-17 17:03:52 -0300

    ✨ feat: save refresh token on sign up

--- server/src/controllers/auth/SignUpController.ts
+++ server/src/controllers/auth/SignUpController.ts
@@ -1,13 +1,19 @@
 import { Request, Response } from 'express'
 import { UsersRepository } from '../../repositories/UsersRepository'
 import { RegisterUserUseCase } from '../../useCases/auth/RegisterUserUseCase'
+import { TokensRepository } from '../../repositories/TokensRepository'
 
 export class SignUpController {
   async handle(request: Request, response: Response) {
     const { name, email, password, password_confirmation } = request.body
 
     const usersRepository = new UsersRepository()
-    const registerUserUseCase = new RegisterUserUseCase(usersRepository)
+    const tokensRepository = new TokensRepository()
+
+    const registerUserUseCase = new RegisterUserUseCase(
+      usersRepository,
+      tokensRepository
+    )
 
     const createdUser = await registerUserUseCase.execute({
       name,
