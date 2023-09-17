c8452c3eaf2fab2b8460343b4d27218c3c2bcfc6
João Pedro <joaopedro.nc@outlook.com>
2023-09-17 16:43:39 -0300

    ✨ feat (utils): generate refresh token

--- server/src/utils/Jwt.ts
+++ server/src/utils/Jwt.ts
@@ -2,21 +2,36 @@ import { sign, verify } from 'jsonwebtoken'
 
 export class Jwt {
   private readonly secretToken = process.env.SECRET_TOKEN
+  private readonly secretRefreshToken = process.env.REFRESH_SECRET_TOKEN
+  public tokenExpiresIn = '1d'
+  public refreshTokenExpiresIn = 30
 
   generateToken(email: string) {
     if (this.secretToken) {
       const token = sign({}, this.secretToken, {
         subject: email,
-        expiresIn: '1d',
+        expiresIn: this.tokenExpiresIn,
       })
 
       return token
     }
   }
 
-  generateRefreshToken(email: string) {}
+  async generateRefreshToken(userId: string) {
+    if (this.secretRefreshToken) {
+      const refreshToken = sign({}, this.secretRefreshToken, {
+        subject: userId,
+        expiresIn: this.refreshTokenExpiresIn,
+      })
+
+      return refreshToken
+    }
+  }
 
   verifyToken(token: string) {
-    if (this.secretToken) verify(token, this.secretToken)
+    if (this.secretToken) {
+      const { sub } = verify(token, this.secretToken)
+      return String(sub)
+    }
   }
 }
