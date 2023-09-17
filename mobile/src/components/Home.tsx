d6d9f2d7a7f813272dacf381c8f7463ec5c1849b
João Pedro <joaopedro.nc@outlook.com>
2023-09-16 22:33:07 -0300

    🐛 fix: resolve home page loading

--- mobile/src/components/Home.tsx
+++ mobile/src/components/Home.tsx
@@ -1,24 +1,87 @@
-import { useAuth } from "../hooks/useAuth";
-import { Box, HStack, Heading, Text, VStack } from "@gluestack-ui/themed";
-import Ionicons from '@expo/vector-icons/Ionicons';
-import { Button } from "./Button";
-import { TouchableOpacity } from "@gorhom/bottom-sheet";
+import { useState } from 'react'
+
+import { FlatList } from 'react-native'
+import { Box, VStack } from '@gluestack-ui/themed'
+import { Header } from './Header'
+import { CourseButton } from './CourseButton'
+import { SubjectCard } from './SubjectCard'
+
+import type { Course } from '../types/course'
+import type { Subject } from '../types/subject'
 
 export function Home() {
-  const { user } = useAuth()
+  const [cousers, setCourses] = useState<Course[]>([
+    {
+      id: '123',
+      title: 'desenvolvimento de sistemas',
+      icon: '',
+    },
+    {
+      id: '456',
+      title: 'automação industrial',
+      icon: '',
+    },
+    {
+      id: '987',
+      title: 'administração',
+      icon: '',
+    },
+  ])
+
+  const [subjects, setSubjects] = useState<Subject[]>([
+    {
+      id: '123',
+      title: 'desenvolvimento de sistemas',
+      image: '',
+      description: '',
+    },
+    {
+      id: '456',
+      title: 'automação industrial',
+      image: '',
+      description: '',
+    },
+    {
+      id: '987',
+      title: 'administração',
+      image: '',
+      description: '',
+    },
+  ])
+
+  const [activeCourseId, setActiveCourseId] = useState('123')
+
+  const icon = 'computer'
 
   return (
-    <Box>
-      <HStack bg="$blue300" justifyContent="space-between" alignItems="center" py={12} px={24}>
-        <Heading fontSize={24} color="$blue700">Etec</Heading>
+    <VStack h="$full" bg="$blue900">
+      <Header />
 
-        <VStack gap={8}>
-          <Text color="$light100">{user?.name}</Text>
-          <TouchableOpacity>
-            <Ionicons name="exit-outline" />
-          </TouchableOpacity>
-        </VStack>
-      </HStack>
-    </Box>
+      <Box mt={24} pl={24}>
+        <FlatList
+          data={cousers}
+          keyExtractor={(item) => item.id}
+          renderItem={({ item }) => (
+            <CourseButton
+              title={item.title}
+              isActive={item.id === activeCourseId}
+            />
+          )}
+          horizontal
+          showsHorizontalScrollIndicator={false}
+        />
+      </Box>
+
+      <Box mt={24} p={24}>
+        <FlatList
+          data={subjects}
+          keyExtractor={(item) => item.id}
+          renderItem={({ item }) => (
+            <SubjectCard title={item.title} icon={'computer'} />
+          )}
+          showsVerticalScrollIndicator={false}
+        />
+      </Box>
+    </VStack>
   )
 }
