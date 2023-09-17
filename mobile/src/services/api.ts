9cd855951a950076d821e5a7da43c86d6148c1d2
João Pedro <joaopedro.nc@outlook.com>
2023-09-16 21:23:36 -0300

    ✨ feat (api): add Authorization Header

--- mobile/src/services/api.ts
+++ mobile/src/services/api.ts
@@ -4,5 +4,9 @@ import axios from 'axios'
 export const api = axios.create({ baseURL: API_URL })
 
 export const getApiErrorMessage = (error: unknown): string => {
-  return axios.isAxiosError(error) ? error.response?.data.errorMessage : null
+  return axios.isAxiosError(error) ? error.response?.data.errorMessage : error
+}
+
+export const addAuthorizationHeader = (token: string) => {
+  if (token) api.defaults.headers['authorization'] = `Bearer ${token}`
 }
