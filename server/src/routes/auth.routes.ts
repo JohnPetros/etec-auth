d15b2227207e44452f0eaf56fac284c15646d62d
João Pedro <joaopedro.nc@outlook.com>
2023-09-17 13:55:32 -0300

    ✨ feat (routes): add auth routes

---
+++ server/src/routes/auth.routes.ts
@@ -0,0 +1,12 @@
+import { Router } from 'express'
+
+import { SignUpController } from '../controllers/auth/SignUpController'
+import { SignInController } from '../controllers/auth/SignInController'
+
+export const authRoutes = Router()
+
+const signUpController = new SignUpController()
+const signInController = new SignInController()
+
+authRoutes.post('/sign_up', signUpController.handle)
+authRoutes.post('/sign_in', signInController.handle)
