b5267a23f90baf71c07e4aa3ef9d346c68d08342
João Pedro <joaopedro.nc@outlook.com>
2023-09-18 19:56:36 -0300

    ✨ feat (utils): add hours to date

--- server/src/utils/Time.ts
+++ server/src/utils/Time.ts
@@ -4,4 +4,8 @@ export class Time {
   addDays(days: number): Date {
     return dayjs().add(days, 'day').toDate()
   }
+
+  addHours(hours: number): Date {
+    return dayjs().add(hours, 'hour').toDate()
+  }
 }
