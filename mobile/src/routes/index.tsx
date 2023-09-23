f62bc9ebfbdaaef8d68d3f07313d9d1d2c8fc5b8
João Pedro <joaopedro.nc@outlook.com>
2023-09-23 15:30:42 -0300

    ✨ feat (routes): render routes type based on user verification status

--- mobile/src/routes/index.tsx
+++ mobile/src/routes/index.tsx
@@ -3,26 +3,38 @@ import { useAuth } from '../hooks/useAuth'
 
 import { NavigationContainer } from '@react-navigation/native'
 
-import { Box } from '@gluestack-ui/themed'
+import { Box, Center, Text } from '@gluestack-ui/themed'
 
 import { AppRoutes } from './app.routes'
 import { AuthRoutes } from './auth.routes'
+import { storage } from '../storage'
 
 export function Routes() {
-  const { user, loadUserData } = useAuth()
+  const { user, isUserDataLoading, loadUserData } = useAuth()
+
+  async function destroy() {
+    await storage.destroyUser()
+    await storage.destroyAllTokens()
+  }
 
   useEffect(() => {
-    console.log({ user })
-
+    // destroy()
     loadUserData()
   }, [])
 
   return (
-    <Box flex={1} bg="blue.900">
-      <NavigationContainer>
-        {/* <AppRoutes /> */}
-        <AuthRoutes />
-      </NavigationContainer>
+    <Box flex={1} bg="blue900">
+      {isUserDataLoading ? (
+        <Center flex={1} bg="blue900">
+          <Text color="$light100" fontWeight="$bold" fontSize="$lg">
+            ...Carregando
+          </Text>
+        </Center>
+      ) : (
+        <NavigationContainer>
+          {user?.is_verified ? <AppRoutes /> : <AuthRoutes />}
+        </NavigationContainer>
+      )}
     </Box>
   )
 }
