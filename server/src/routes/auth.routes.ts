28516e6f3b0050e0e7ece28017ecceb7a4796012
João Pedro <joaopedro.nc@outlook.com>
2023-09-23 15:26:13 -0300

    ✨ feat (routes): add confirm email route

--- server/src/routes/auth.routes.ts
+++ server/src/routes/auth.routes.ts
@@ -3,10 +3,12 @@ import { Router } from 'express'
 import { SignUpController } from '../controllers/auth/SignUpController'
 import { SignInController } from '../controllers/auth/SignInController'
 import { RefreshTokenController } from '../controllers/auth/RefreshTokenController'
-import { checkAuthentication } from '../middlewares/checkAuthentication'
+import { ConfirmUserEmailController } from '../controllers/auth/ConfirmUserEmailController'
 import { SendForgotPasswordMailController } from '../controllers/auth/SendForgotPasswordMailController'
 import { ResetPasswordController } from '../controllers/auth/ResetPasswordController'
 import { SignOutController } from '../controllers/auth/SignOutUserController'
+
+import { checkAuthentication } from '../middlewares/checkAuthentication'
 
 export const authRoutes = Router()
 
@@ -14,17 +16,23 @@ const signUpController = new SignUpController()
 const signInController = new SignInController()
 const signOutController = new SignOutController()
 const refreshTokenController = new RefreshTokenController()
+const confirmUserEmailController = new ConfirmUserEmailController()
 const sendForgotPasswordMailController = new SendForgotPasswordMailController()
 const resetPasswordController = new ResetPasswordController()
 
 authRoutes.post('/sign_up', signUpController.handle)
 authRoutes.post('/sign_in', signInController.handle)
-authRoutes.post('/sign_out/:user_id', checkAuthentication, signOutController.handle)
+authRoutes.post(
+  '/sign_out/:user_id',
+  checkAuthentication,
+  signOutController.handle
+)
 authRoutes.post(
   '/refresh_token',
   checkAuthentication,
   refreshTokenController.handle
 )
+authRoutes.post('/confirm_email', confirmUserEmailController.handle)
 authRoutes.post(
   '/send_forgot_password_mail',
   checkAuthentication,
