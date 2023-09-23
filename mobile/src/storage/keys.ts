59a44db5edab60655cf0035b190961e54e9b5321
João Pedro <joaopedro.nc@outlook.com>
2023-09-23 15:22:15 -0300

    ✨ feat (storage): add refresh token key

--- mobile/src/storage/keys.ts
+++ mobile/src/storage/keys.ts
@@ -1,8 +1,6 @@
 const PREFIX = '@etec-auth'
 const USER_KEY = `${PREFIX}:user`
 const TOKEN_KEY = `${PREFIX}:token`
+const REFRESH_TOKEN_KEY = `${PREFIX}:refresh_token`
 
-export {
-  USER_KEY,
-  TOKEN_KEY
-}
+export { USER_KEY, TOKEN_KEY, REFRESH_TOKEN_KEY }
