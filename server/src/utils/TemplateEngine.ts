66cb64481d3a81e4af1fd3c5e69678d14b8e1455
João Pedro <joaopedro.nc@outlook.com>
2023-09-23 10:14:42 -0300

    ✨ feat (TemplateEngine): get template path

--- server/src/utils/TemplateEngine.ts
+++ server/src/utils/TemplateEngine.ts
@@ -1,7 +1,14 @@
 import handlebars from 'handlebars'
+import { File } from './File'
 
 export class TemplateEngine {
   compile(fileContent: string) {
     return handlebars.compile(fileContent)
+  }
+
+  getTemplatePath(folder: string, fileName: string) {
+    const file = new File()
+
+    return file.resolvePath(__dirname, '..', '..', folder, fileName)
   }
 }
