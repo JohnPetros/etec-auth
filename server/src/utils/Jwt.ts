cfd00b366a53298a642cbdd96a798d23c1c1a844
João Pedro <joaopedro.nc@outlook.com>
2023-09-24 12:37:22 -0300

    ✨ feat (jwt): handle jwt verification errors

--- server/src/utils/Jwt.ts
+++ server/src/utils/Jwt.ts
@@ -1,5 +1,6 @@
 import { sign, verify } from 'jsonwebtoken'
 import { authConfig } from '../configs/authConfig'
+import { AppError } from './AppError'
 
 export class Jwt {
   private readonly secretToken = authConfig.secretToken
@@ -8,6 +9,15 @@ export class Jwt {
   public readonly tokenExpiresIn = authConfig.tokenExpiresIn
   public readonly emailTokenExpiresIn = authConfig.emailTokenExpiresIn
   public readonly refreshTokenExpiresIn = authConfig.refreshTokenExpiresIn
+  private readonly errors: Record<string, string> = {
+    'jwt expired': 'expirado',
+    'jwt malformed': 'inválido',
+  }
+
+  private handleVerificationError(error: Error, tokenType: string) {
+    const errorMessage = this.errors[error.message] ?? 'Token inválido'
+    throw new AppError(`${tokenType} ${errorMessage}`, 401)
+  }
 
   generateAuthToken(userId: string) {
     if (this.secretToken) {
@@ -44,22 +54,34 @@ export class Jwt {
 
   verifyAuthToken(token: string) {
     if (this.secretToken) {
-      const { sub } = verify(token, this.secretToken)
-      return String(sub)
+      try {
+        const { sub } = verify(token, this.secretToken)
+        return String(sub)
+      } catch (error) {
+        this.handleVerificationError(error as Error, 'Token')
+      }
     }
   }
 
   verifyEmailToken(token: string) {
     if (this.secretEmailToken) {
-      const { sub } = verify(token, this.secretEmailToken)
-      return String(sub)
+      try {
+        const { sub } = verify(token, this.secretEmailToken)
+        return String(sub)
+      } catch (error) {
+        this.handleVerificationError(error as Error, 'Token de e-mail')
+      }
     }
   }
 
   verifyRefreshToken(refreshToken: string) {
     if (this.secretRefreshToken) {
-      const { sub } = verify(refreshToken, this.secretRefreshToken)
-      return String(sub)
+      try {
+        const { sub } = verify(refreshToken, this.secretRefreshToken)
+        return String(sub)
+      } catch (error) {
+        this.handleVerificationError(error as Error, 'Refresh token')
+      }
     }
   }
 }
