576db06b9d0ec78694be3dfe894f4182c0afc010
João Pedro <joaopedro.nc@outlook.com>
2023-09-24 12:40:49 -0300

    ✨ feat: add refresh token config to api service

--- mobile/src/services/api.ts
+++ mobile/src/services/api.ts
@@ -1,5 +1,14 @@
-import axios from 'axios'
+import axios, { AxiosError } from 'axios'
+
 import { API_URL } from '@env'
+
+import { storage } from '../storage'
+
+import type { RefreshTokenResponse } from '../types/authResponse'
+
+type ApiData = {
+  errorMessage: string
+}
 
 export const api = axios.create({ baseURL: API_URL })
 
@@ -7,6 +16,86 @@ export const getApiErrorMessage = (error: unknown): string => {
   return axios.isAxiosError(error) ? error.response?.data.errorMessage : error
 }
 
-export const addAuthorizationHeader = (token: string) => {
+export const setAuthorizationHeader = (token: string) => {
   if (token) api.defaults.headers['authorization'] = `Bearer ${token}`
 }
+
+type FailRequest = {
+  onSuccess: (newToken: string) => void
+  onFailure: (error: AxiosError) => void
+}
+
+let isRefreshingToken = false
+let failRequestsQueue: FailRequest[] = []
+
+async function refreshToken() {
+  try {
+    const currentRefreshToken = await storage.getRefreshToken()
+
+    const {
+      data: { token, refreshToken },
+    } = await api.post<RefreshTokenResponse>('auth/refresh_token', {
+      refresh_token: currentRefreshToken,
+    })
+
+    await Promise.all([
+      storage.saveToken(token),
+      storage.saveRefreshToken(refreshToken),
+    ])
+
+    setAuthorizationHeader(token)
+
+    failRequestsQueue.forEach((request) => request.onSuccess(token))
+    failRequestsQueue = []
+  } catch (error) {
+    failRequestsQueue.forEach((request) =>
+      request.onFailure(error as AxiosError)
+    )
+    failRequestsQueue = []
+  } finally {
+    isRefreshingToken = false
+  }
+}
+
+api.interceptors.response.use(
+  (response) => {
+    return response
+  },
+  (error: AxiosError) => {
+    const { errorMessage } = error.response?.data as ApiData
+    const originalErrorConfig = error.config
+
+    async function executeRefreshToken() {
+      await refreshToken()
+    }
+
+    if (errorMessage === 'Token expirado') {
+      if (!isRefreshingToken) executeRefreshToken()
+
+      return new Promise((resolve, reject) => {
+        failRequestsQueue.push({
+          onSuccess: (newToken: string) => {
+            if (originalErrorConfig) {
+              originalErrorConfig.headers[
+                'Authorization'
+              ] = `Bearer ${newToken}`
+
+              resolve(api(originalErrorConfig))
+            }
+          },
+          onFailure: (error: AxiosError) => {
+            reject(error)
+          },
+        })
+      })
+    }
+
+    async function destroyUserData() {
+      await Promise.all([storage.destroyAllTokens(), storage.destroyUser()])
+    }
+
+    destroyUserData()
+
+    return Promise.reject(error)
+  }
+)
