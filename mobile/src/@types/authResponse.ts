278c1e596e7543009d8f18bcca54661877777ef8
João Pedro <joaopedro.nc@outlook.com>
2023-09-24 14:00:20 -0300

    🚚 file: resolve auth response file path

---
+++ mobile/src/@types/authResponse.ts
@@ -0,0 +1,28 @@
+import { User } from './user'
+
+export type SignUpResponse = {
+  user: User
+  emailToken: string
+  refreshToken: string
+  message: string
+  errorMessage: string | string[]
+}
+
+export type SignInResponse = {
+  user: User
+  token: string
+  refreshToken: string
+  errorMessage: string | string[]
+}
+
+export type ConfirmEmailResponse = {
+  user: User
+  token: string
+  refreshToken: string
+  errorMessage: string | string[]
+}
+
+export type RefreshTokenResponse = {
+  token: string
+  refreshToken: string
+}
