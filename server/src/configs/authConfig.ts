d9d8574abc1f1dec69748e448cd65ffc94acb080
João Pedro <joaopedro.nc@outlook.com>
2023-09-24 14:01:37 -0300

    🔧 chore: set token expires in

--- server/src/configs/authConfig.ts
+++ server/src/configs/authConfig.ts
@@ -2,7 +2,7 @@ export const authConfig = {
   secretToken: process.env.SECRET_TOKEN,
   secretEmailToken: process.env.EMAIL_SECRET_TOKEN,
   secretRefreshToken: process.env.REFRESH_SECRET_TOKEN,
-  tokenExpiresIn: '1d',
-  emailTokenExpiresIn: '1h',
+  tokenExpiresIn: '30m',
+  emailTokenExpiresIn: '5m',
   refreshTokenExpiresIn: 30,
 }
