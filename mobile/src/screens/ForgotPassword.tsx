b90111326ce71381b0f7b25d7a9a7c1c595a2bd2
João Pedro <joaopedro.nc@outlook.com>
2023-09-24 14:02:04 -0300

    🚧 in progress: add ForgotPassword base component

--- mobile/src/screens/ForgotPassword.tsx
+++ mobile/src/screens/ForgotPassword.tsx
@@ -1,5 +1,30 @@
-import { Box, Text } from '@gluestack-ui/themed'
+import { Box, Center, Text } from '@gluestack-ui/themed'
+import { Input } from '../components/Input'
+import { useState } from 'react'
+import { useForm } from 'react-hook-form'
 
-export function ForgotPassword() {
-  return <Text>ForgotPassword</Text>
+export function ForgotPassword({}) {
+  const {register} = useForm()
+  const [email, setEmail] = useState('')
+
+
+  function handleEmailChange(value: string) {
+
+  }
+
+  return (
+    <Box flex={1} bg="$blue900" p={24}>
+      <Center>
+        <Input
+          type="email"
+          label="E-mail"
+          placeholder="seu e-mail"
+          value={email}
+          errorMessage={''}
+          onChange={handleEmailChange}
+          {...register('email')}
+        />
+      </Center>
+    </Box>
+  )
 }
