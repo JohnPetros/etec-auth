c1d7fe42daab95bfdc567bd691adb89bb96d665a
João Pedro <joaopedro.nc@outlook.com>
2023-09-25 13:44:10 -0300

    ✨ feat (utils): add course icons

---
+++ mobile/src/utils/course-icons.ts
@@ -0,0 +1,7 @@
+import { LucideIcon, Computer, Factory, Building2 } from 'lucide-react-native'
+
+export const COURSE_ICONS: Record<string, LucideIcon> = {
+  'desenvolvimento de sistemas': Computer,
+  'automação industrial': Factory,
+  administração: Building2,
+}
