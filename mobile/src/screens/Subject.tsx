73ff4fb0b68b4208bd1bde4012c06b34b074896b
João Pedro <joaopedro.nc@outlook.com>
2023-09-25 13:45:46 -0300

    ✨ feat (mobile): fetch subject data in subjec screen

--- mobile/src/screens/Subject.tsx
+++ mobile/src/screens/Subject.tsx
@@ -1,8 +1,24 @@
 import { useRoute } from '@react-navigation/native'
 
-import { Box, HStack, Heading, Icon, Image, Text, VStack } from '@gluestack-ui/themed'
+import {
+  Box,
+  HStack,
+  Heading,
+  Icon,
+  Image,
+  Text,
+  VStack,
+  Button,
+  Pressable,
+  Center,
+} from '@gluestack-ui/themed'
 import { Header } from '../components/Header'
-import { AlignCenter } from 'lucide-react-native'
+import { ChevronLeft, AlignCenter } from 'lucide-react-native'
+import { Subject as SubjectData } from '../@types/subject'
+import { useEffect, useState } from 'react'
+import { api } from '../services/api'
+import { Loading } from '../components/Loading'
+import { COURSE_ICONS } from '../utils/course-icons'
 
 type RouteParams = {
   id: string
@@ -10,44 +26,86 @@ type RouteParams = {
 
 export function Subject() {
   const route = useRoute()
+  const [subject, setSubject] = useState<SubjectData | null>(null)
+  const [isLoading, setIsLoading] = useState(false)
 
   const { id } = route.params as RouteParams
+
+  async function fetchSubjectData() {
+    setIsLoading(true)
+
+    try {
+      const response = await api.get('subjects/' + id)
+      const subject = await response.data
+
+      console.log(subject.course.title)
+
+      setSubject(subject)
+    } catch (error) {
+    } finally {
+      setIsLoading(false)
+    }
+  }
+
+  useEffect(() => {
+    if (id) fetchSubjectData()
+  }, [id])
 
   return (
     <VStack flex={1} bg="$blue900">
       <Header />
 
-      <VStack p={24}>
-        <Heading color="$light100" fontSize={24} fontWeight="bold">
-          Banco de dados
-        </Heading>
+      {!subject || isLoading ? (
+        <Center flex={1}>
+          <Loading />
+        </Center>
+      ) : (
+        <VStack pt={12} px={24}>
+          <HStack alignItems="center" justifyContent="space-between">
+            <Pressable>
+              <Icon as={ChevronLeft} color="$blue300" size={32} />
+            </Pressable>
 
-        <Box mt={24}>
-          {/* <Image source="../../../server/src/storage/web-development.svg" /> */}
-        </Box>
-
-        <Box p={12} bg="$blue700" rounded={8}>
-          <VStack>
             <HStack gap={8} alignItems="center">
-              <Icon as={AlignCenter} color="$blue300" size={24} />
-              <Heading
+              <Icon
+                as={COURSE_ICONS[subject.course.title.toLowerCase()]}
                 color="$blue300"
-                letterSpacing={1.2}
-                fontSize={18}
-                fontWeight="bold"
-              >
-                Descrição
-              </Heading>
+                size={24}
+              />
+              <Text color="$blue300" fontSize={16} fontWeight="bold">
+                {subject.course.title}
+              </Text>
             </HStack>
-            <Text color="$light100" lineHeight={24} mt={4}>
-              Lorem ipsum dolor sit amet consectetur, adipisicing elit.
-              Consequuntur enim facilis, mollitia quas reprehenderit ea
-              molestiae provident? Esse reprehenderit, nulla perferendis,
-              voluptates ut nemo vitae fuga numquam magnam, placeat odit!
-            </Text>
-          </VStack>
-        </Box>
-      </VStack>
+          </HStack>
+
+          <Heading color="$light100" mt={24} fontSize={24} fontWeight="bold">
+            {subject.title}
+          </Heading>
+
+          <Box mt={24}>
+            {/* <Image source="../../../server/src/storage/web-development.svg" /> */}
+          </Box>
+
+          <Box p={12} bg="$blue700" rounded={8}>
+            <VStack>
+              <HStack gap={8} alignItems="center">
+                <Icon as={AlignCenter} color="$blue300" size={24} />
+                <Heading
+                  color="$blue300"
+                  letterSpacing={1.2}
+                  fontSize={18}
+                  fontWeight="bold"
+                >
+                  Descrição
+                </Heading>
+              </HStack>
+              <Text color="$light100" lineHeight={24} mt={4}>
+                {subject.description}
+              </Text>
+            </VStack>
+          </Box>
+        </VStack>
+      )}
     </VStack>
   )
 }
