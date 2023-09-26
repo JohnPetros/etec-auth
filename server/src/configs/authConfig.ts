7eb54255de47f800693f2156d61eb5ec0a55acdf
João Pedro <joaopedro.nc@outlook.com>
2023-09-26 13:05:04 -0300

    🔧 chore: add password token config

--- server/src/configs/authConfig.ts
+++ server/src/configs/authConfig.ts
@@ -1,8 +1,10 @@
 export const authConfig = {
   secretToken: process.env.SECRET_TOKEN,
   secretEmailToken: process.env.EMAIL_SECRET_TOKEN,
+  secretPasswordToken: process.env.PASSWORD_SECRET_TOKEN,
   secretRefreshToken: process.env.REFRESH_SECRET_TOKEN,
   tokenExpiresIn: '30m',
   emailTokenExpiresIn: '5m',
+  passwordTokenExpiresIn: '5m',
   refreshTokenExpiresIn: 30,
 }
