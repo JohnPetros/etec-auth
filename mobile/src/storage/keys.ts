ac437b34aa558e11b8a0161d5389639d6438cf16
João Pedro <joaopedro.nc@outlook.com>
2023-09-26 15:42:30 -0300

    🔧 chore: add USER_EMAIL_KEY

--- mobile/src/storage/keys.ts
+++ mobile/src/storage/keys.ts
@@ -1,6 +1,7 @@
 const PREFIX = '@etec-auth'
 const USER_KEY = `${PREFIX}:user`
+const USER_EMAIL_KEY = `${PREFIX}:user_email`
 const TOKEN_KEY = `${PREFIX}:token`
 const REFRESH_TOKEN_KEY = `${PREFIX}:refresh_token`
 
-export { USER_KEY, TOKEN_KEY, REFRESH_TOKEN_KEY }
+export { USER_KEY, USER_EMAIL_KEY, TOKEN_KEY, REFRESH_TOKEN_KEY }
